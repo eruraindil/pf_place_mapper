d408e320f64f74d0b7537393abcfbb06a1e22b75
Matthew Roberts <matt@mroberts.me>
2023-10-10 18:42:43 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pf_place_mapper
-Visualize pixelfed locations on a map to find the closest one to your photo's actual location.
+Visualize pixelfed locations on a map to find the closest one to your photos actual location.
+
+I am currently hardcoding locations to Canada only to reduce the number of markers added to the map, loading up more causes lag. This could be improved but this is a dirty script to fix the problem of not knowing which place is closest to my photos actual location.
---
+++ place_mapper.js
@@ -0,0 +1,90 @@
+const file_cities = "https://raw.githubusercontent.com/pixelfed/pixelfed/dev/storage/app/cities.json";
+
+Promise.all([
+    d3.json(file_cities)
+]).then(function(data) {
+
+    const cities = data[0].filter((city => city.country == "CA"));
+    let pins = [];
+    let clickPin;
+    let clickPath;
+
+    // replace "toner" here with "terrain" or "watercolor"
+    var map = new L.Map("map", {
+        center: new L.LatLng(59.400, -95.933),
+        zoom: 4
+    });
+    var layer = new L.StamenTileLayer("toner");
+    map.addLayer(layer);
+
+    for (let i = 0; i < cities.length; i++) {
+        let city = cities[i];
+        let lat = city.lat;
+        let lng = city.lng;
+
+        pins[lat + "_" + lng] = L.marker(
+                [lat, lng],
+                {icon: L.divIcon({ className: "map-icon" })}
+            ).addTo(map)
+            .bindPopup(city.name);
+    }
+
+    map.on("click", function(ev) {
+        if (clickPin) {
+            map.removeLayer(clickPin);
+        }
+        if (clickPath) {
+            map.removeLayer(clickPath);
+        }
+
+        let = prevClosest = document.querySelector(".map-icon-closest");
+        if (prevClosest) {
+            prevClosest.classList.remove('map-icon-closest');
+        }
+
+        clickPin = L.marker(
+            ev.latlng,
+            {icon: L.divIcon({ className: "map-icon map-icon-click" })}
+        ).addTo(map);
+
+        let latSorted = cities.toSorted(function (a, b) {
+            let aDist = Math.abs(ev.latlng.lat - a.lat);
+            let bDist = Math.abs(ev.latlng.lat - b.lat);
+
+            if (aDist < bDist) {
+                return -1;
+            } else if (aDist > bDist) {
+                return 1;
+            }
+            return 0;
+        });
+
+        // get shortest distance between coordinates https://stackoverflow.com/a/55083771
+        let closestPin = latSorted[0];
+        let shortestDist = coordSquared(ev.latlng, latSorted[0]);
+        console.log(ev.latlng, closestPin, shortestDist);
+        for (let i = 0; i < latSorted.length; i++) {
+            let dist = coordSquared(ev.latlng, latSorted[i]);
+            if (dist < shortestDist) {
+                closestPin = latSorted[i];
+                shortestDist = dist;
+                console.log(ev.latlng, closestPin, shortestDist);
+            }
+        }
+
+        clickPath = L.polyline(
+            [ev.latlng, [closestPin.lat, closestPin.lng]],
+            {color: "red"}
+        ).addTo(map);
+
+        pins[closestPin.lat + "_" + closestPin.lng].setIcon(L.divIcon({ className: "map-icon map-icon-closest" }));
+        pins[closestPin.lat + "_" + closestPin.lng].openPopup();
+        map.panTo([closestPin.lat, closestPin.lng]);
+    });
+});
+
+function coordSquared(a, b) {
+    let diffLat = Math.abs(a.lat - b.lat);
+    let diffLng = Math.abs(a.lng - b.lng);
+    return (diffLat*diffLat) + (diffLng*diffLng);
+}
